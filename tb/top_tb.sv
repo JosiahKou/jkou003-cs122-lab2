7b21319a87958eb61a9a24b5553e179bddda6499
uokm <141180878+uokm@users.noreply.github.com>
2026-04-07 19:10:34 -0700

    update

--- tb/top_tb.sv
+++ tb/top_tb.sv
@@ -1,27 +1,32 @@
 `include "src/top.sv"
 
 module top_tb;
-logic [3:0] bcd;
-logic [6:0] seg7;
+reg [3:0] val1, val2;
+wire [7:0] seg7;
 
-top uut 
-(
-    .bcd(bcd),
-    .seg7(seg7)
-);
+top uut (.val1(val1), .val2(val2), .seg7(seg7));
 
 initial begin
     $dumpfile("build/top.vcd");
     $dumpvars(0, top_tb);
-end
-
-initial begin
-    for (int i = 0; i < 16; i++) begin
-        bcd = i;
-        #10;
-    end
-    #10;
+    
+    $display("val1 val2 sum seg7[6:0] DP");
+    $display("---- ---- --- --------- --");
+    
+    // Test normal addition
+    val1 = 5; val2 = 3; #10;
+    $display(" %2d   %2d   %2d    %b    %b", val1, val2, val1+val2, seg7[6:0], seg7[7]);
+    
+    // Test overflow
+    val1 = 9; val2 = 9; #10;
+    $display(" %2d   %2d   %2d    %b    %b", val1, val2, val1+val2, seg7[6:0], seg7[7]);
+    
+    val1 = 15; val2 = 1; #10;
+    $display(" %2d   %2d   %2d    %b    %b", val1, val2, val1+val2, seg7[6:0], seg7[7]);
+    
+    val1 = 8; val2 = 8; #10;
+    $display(" %2d   %2d   %2d    %b    %b", val1, val2, val1+val2, seg7[6:0], seg7[7]);
+    
     $finish;
 end
-
 endmodule
